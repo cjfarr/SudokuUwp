4cd16e71ce307392bfa579446af521ca43c271b3
CJ Farr <chrisjfarr@outlook.com>
2022-01-31 08:27:26 -0600

    made a couple of aesthetic changes around the error viewing feature.

---
+++ Sudoku/Sudoku/Converters/CellForegroundConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace Sudoku.Converters
+{
+    using System;
+    using Windows.UI;
+    using Windows.UI.Xaml.Data;
+    using Windows.UI.Xaml.Media;
+
+    public class CellForegroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            int selectionCode = int.Parse(value.ToString());
+
+            switch (selectionCode)
+            {
+                case 3:
+                    return new SolidColorBrush(Colors.White);
+            }
+
+            return new SolidColorBrush(Colors.Black);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
